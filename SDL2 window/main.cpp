1bd21acfa98a8d2f54e19f103dc2ed360e4a945c
Aliaksandr Karpovich <alexsure.k@gmail.com>
2022-12-21 11:33:50 +0300

    Initial Commit

---
+++ SDL2 window/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SDL2 window
+//
+//  Created by Aliaksandr on 21.12.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
